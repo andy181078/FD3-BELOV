df6f2ca4bfc3c9f54298b9f5dddd907ca8b14f33
andy181078 <andrbelov18@gmail.com>
2021-10-01 12:16:19 +0300

    на проверку

--- ishop/IshopBlock.js
+++ ishop/IshopBlock.js
@@ -18,20 +18,21 @@ var VotesBlock = React.createClass({
           React.DOM.span({className:'Count1'},good.count),
         );
       goodsCode.push(goodCode);
-      for ( var a=0; a<this.props.goods1.length; a++ ) {
-        //function f(v,i,a) {
-        var good1=this.props.goods1[a];
+      //for ( var a=0; a<this.props.goods1.length; a++ ) {
+        var good1=this.props.goods1;
+        function f(v,i,a) {
         var goodCode1=        
-          React.DOM.div({key:good1.code,className:'Good'},
-            React.DOM.span({className:'Text'},good1.text),
-            React.DOM.span({className:'Count'},good1.ul1),
+          React.DOM.div({key:v.code,className:'Good'},
+            React.DOM.span({className:'Text'},v.text),
+            React.DOM.span({className:'Count'},v.ul1),
             //React.DOM.span({className:'Count'},
             //React.DOM.a({href:'good1.ul1'})),
-            React.DOM.span({className:'Count'},good1.price),
-            React.DOM.span({className:'Count'},good1.count),
+            React.DOM.span({className:'Count'},v.price),
+            React.DOM.span({className:'Count'},v.count),
           );
         goodsCode.push(goodCode1);
       }
+      good1.forEach(f);
       return React.DOM.div( {className:'GoodsBlock'}, 
         React.DOM.div( {className:'Name'}, this.props.name ),
         React.DOM.div( {className:'Goods'}, goodsCode ),
