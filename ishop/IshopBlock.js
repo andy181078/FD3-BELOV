287d74ff490cf8e9831528a6aa8216f3662e1596
andy181078 <andrbelov18@gmail.com>
2021-10-01 12:03:17 +0300

    вариант 3

--- ishop/IshopBlock.js
+++ ishop/IshopBlock.js
@@ -9,26 +9,28 @@ var VotesBlock = React.createClass({
     render: function() {
   
       var goodsCode=[];
-      var good1=this.props.goods[0];
-      var goodCode1=        
-        React.DOM.div({key:good1.code,className:'Good1'},
-          React.DOM.span({className:'Text1'},good1.text),
-          React.DOM.span({className:'Count1'},good1.ul1),
-          React.DOM.span({className:'Count1'},good1.price),
-          React.DOM.span({className:'Count1'},good1.count),
+      var good=this.props.goods[0];
+      var goodCode=        
+        React.DOM.div({key:good.code,className:'Good1'},
+          React.DOM.span({className:'Text1'},good.text),
+          React.DOM.span({className:'Count1'},good.ul1),
+          React.DOM.span({className:'Count1'},good.price),
+          React.DOM.span({className:'Count1'},good.count),
         );
-      goodsCode.push(goodCode1);
-      for ( var a=1; a<this.props.goods.length; a++ ) {
-        var good=this.props.goods[a];
-        var goodCode=        
-          React.DOM.div({key:good.code,className:'Good'},
-            React.DOM.span({className:'Text'},good.text),
-            //React.DOM.span({className:'Count'},good.ul1),
-            React.DOM.span({className:'Count'},good.ul1),
-            React.DOM.span({className:'Count'},good.price),
-            React.DOM.span({className:'Count'},good.count),
+      goodsCode.push(goodCode);
+      for ( var a=0; a<this.props.goods1.length; a++ ) {
+        //function f(v,i,a) {
+        var good1=this.props.goods1[a];
+        var goodCode1=        
+          React.DOM.div({key:good1.code,className:'Good'},
+            React.DOM.span({className:'Text'},good1.text),
+            React.DOM.span({className:'Count'},good1.ul1),
+            //React.DOM.span({className:'Count'},
+            //React.DOM.a({href:'good1.ul1'})),
+            React.DOM.span({className:'Count'},good1.price),
+            React.DOM.span({className:'Count'},good1.count),
           );
-        goodsCode.push(goodCode);
+        goodsCode.push(goodCode1);
       }
       return React.DOM.div( {className:'GoodsBlock'}, 
         React.DOM.div( {className:'Name'}, this.props.name ),
